c0224edc02b46da8ec854db37f97f13c404b4e2c
Rafael Choinhet <70548615+rxonhe@users.noreply.github.com>
2022-08-26 08:14:22 -0300

    changing function use

--- functions/standart_functions.py
+++ functions/standart_functions.py
@@ -44,7 +44,7 @@ def add_n_numbers_v2(*args):
 
 if __name__ == '__main__':
     one, two, three, four = 1, 2, 3, 4
-    print(f"f(one) = 2 + one = {f(one)}")
+    print(f"f(one) = {f(one)}")
     print(f"one + two = {add_2_numbers(one, two)}")
     print(f"one + two + three = {add_n_numbers(one, two, three)}")
     print(f"one + two + three + four = {add_n_numbers_v2(one, two, three, four)}")
