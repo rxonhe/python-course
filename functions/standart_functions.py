cb9701ddb9e447fc9a71d51947f26a501b35b219
Rafael Choinhet <70548615+rxonhe@users.noreply.github.com>
2022-08-26 08:07:21 -0300

    adding docstrings

--- functions/standart_functions.py
+++ functions/standart_functions.py
@@ -1,15 +1,28 @@
 def f(x):
-    """Math function repr"""
+    """
+    Math function repr
+    :param x -> number to be operated
+    :return 2+x
+    """
     return 2 + x
 
 
 def add_2_numbers(a, b):
-    """Sum of two numbers"""
+    """
+    Sum of two numbers
+    :param a -> first number
+    :param b -> second number
+    :return 2+x
+    """
     return a + b
 
 
 def add_n_numbers(*args):
-    """Sum of n numbers"""
+    """
+    Sum of n numbers
+    :param *args -> n numbers to be operated
+    :return total -> sum of all numbers
+    """
     total = 0
     for number in args:
         total += number
@@ -17,7 +30,11 @@ def add_n_numbers(*args):
 
 
 def add_n_numbers_v2(*args):
-    """Sum of n numbers"""
+    """
+    Sum of n numbers
+    :param *args -> n numbers to be operated
+    :return total -> sum of all numbers
+    """
     return sum(args)
 
 
